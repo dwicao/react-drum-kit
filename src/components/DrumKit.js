863b6653595fbc50c69f50e1522dde4c4ecaf808
chrome <lutfiandwica@gmail.com>
2016-12-12 09:55:12 +0700

    added 8 another DrumKit

--- src/components/DrumKit.js
+++ src/components/DrumKit.js
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import '../styles/DrumKit.css';
 
 export default class DrumKit extends Component {
   constructor() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,93 @@
 import React, { Component } from 'react';
 import DrumKit from './DrumKit';
+
+import '../styles/Home.css';
+
 import clap from '../sounds/clap.wav';
-import '../styles/main.css';
+import hihat from '../sounds/hihat.wav';
+import kick from '../sounds/kick.wav';
+import openhat from '../sounds/openhat.wav';
+import boom from '../sounds/boom.wav';
+import ride from '../sounds/ride.wav';
+import snare from '../sounds/snare.wav';
+import tom from '../sounds/tom.wav';
+import tink from '../sounds/tink.wav';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        <DrumKit
-          className="snare"
-          smallText="A"
-          bigText="Snare"
-          code={65}
-          source={clap}
-        />
+        <div className="one">
+          <DrumKit
+            className="drumKit clap"
+            smallText="a"
+            bigText="clap"
+            code={65}
+            source={clap}
+          />
+          <DrumKit
+            className="drumKit hihat"
+            smallText="s"
+            bigText="hihat"
+            code={83}
+            source={hihat}
+          />
+          <DrumKit
+            className="drumKit kick"
+            smallText="d"
+            bigText="kick"
+            code={68}
+            source={kick}
+          />
+        </div>
+        <div className="two">
+          <DrumKit
+            className="drumKit openhat"
+            smallText="f"
+            bigText="openhat"
+            code={70}
+            source={openhat}
+          />
+          <DrumKit
+            className="drumKit boom"
+            smallText="g"
+            bigText="boom"
+            code={71}
+            source={boom}
+          />
+          <DrumKit
+            className="drumKit ride"
+            smallText="h"
+            bigText="ride"
+            code={72}
+            source={ride}
+          />
+        </div>
+        <div className="three">
+          <DrumKit
+            className="drumKit snare"
+            smallText="j"
+            bigText="snare"
+            code={74}
+            source={snare}
+          />
+          <DrumKit
+            className="drumKit tom"
+            smallText="k"
+            bigText="tom"
+            code={75}
+            source={tom}
+          />
+          <DrumKit
+            className="drumKit tink"
+            smallText="l"
+            bigText="tink"
+            code={76}
+            source={tink}
+          />
+        </div>
         <p>Click Button or Press "A" on Keyboard to Play Sound</p>
-      </div>
+    </div>
     );
   }
 }
